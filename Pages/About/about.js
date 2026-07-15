7130bd11f44f2d6307f673e504873636e226e55b
azamatovnakumush2002-lang <azamatovnakumush2002@gmail.com>
2026-07-15 13:02:05 +0500

    f

---
+++ Pages/About/about.js
@@ -0,0 +1,8 @@
+// let section1 = document.querySelector("#section1");
+// let section1_info = document.querySelector(".section1-info");
+
+// let section1_image = document.querySelector(".section1-image");
+// let span = document.createElement("span");
+// span.textContent = "ABOUT US";
+// section1_info.appendChild(span);
+// span.classList.add("spann");
