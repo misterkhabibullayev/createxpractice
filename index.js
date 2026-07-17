786f41652e3a97985c15b927738ea624542ee4a4
misterkhabibullayev <misterkhabibullayev1@gmail.com>
2026-07-17 14:05:11 +0500

    feet: multi language added, language btn added

--- index.js
+++ index.js
@@ -1,12 +1,396 @@
+// import { allCourses } from "./AllCourses";
 
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
+const translateData = {
+    en: {
+        langTitle: "EN",
+        flag: "Image/HomePagePhoto/twemoji--flag-united-states.svg",
+        navAbout: "About Us",
+        navCourses: "Courses",
+        navEvents: "Events",
+        navBlog: "Blog",
+        navContacts: "Contacts",
+        getConsultation: "Get consultation",
+        loginRegister: "Log in / Register",
+        playShowreel: "Play showreel",
+        heroSectionH1: "Enjoy studying with Createx Online Courses",
+        exploreCourses: "Explore courses",
+        studentGraduet: "Students graduated",
+        commlatedCourses: "Completed courses",
+        qualifiedTutors: "Qualified tutors",
+        experienceYears: "Years of experience",
+        whoweare: "Who we are",
+        whyCreateX: "Why Createx?",
+        why1: "A fermentum in morbi pretium aliquam adipiscing donec tempus.",
+        why2: "Vulputate placerat amet pulvinar lorem nisl.",
+        why3: "Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.",
+        why4: "Etiam duis lobortis in fames ultrices commodo nibh.",
+        why5: "Tincidunt sagittis neque sem ac eget.",
+        why6: "Ultricies amet justo et eget quisque purus vulputate dapibus tortor.",
+        readytolearn: "Ready to learn?",
+        featuredCourses: "Featured Courses",
+        viewAllCourses: "View all courses",
+        ourbenefits: "Our benefits",
+        thatshowwedoit: "That’s how we do it",
+        experiencedTutors: "Experienced Tutors",
+        feedbackSupports: "Feedback & Support",
+        onlineLibrary: "24/7 Online Library",
+        community: "Community",
+        ourevents: "Our Events",
+        lecturesWorkshop: "Lectures & workshops",
+        doyouwatnmore: "Do you want more?",
+        exploreAllEvents: "Explore all events",
+        createXCertificate: "Createx Certificate",
+        createXCertificateH1: "Your expertise will be confirmed",
+        createXCertificateP:
+            "We are accredited by international professional organizations and institutes:",
+        bestTutorsHere: "Best tutors are all here",
+        meetOurTema: "Meet our team",
+        testimonials: "TESTIMONIALS",
+        whatOurStudentsSay: "What our students say",
+        ourBlog: "Our blog",
+        latestPosts: "Latest posts",
+        goBlog: "Go to blog",
+        dontmiss: "Don’t miss anything",
+        subscribeTitle: "Subscribe to the Createx School announcements",
+        subscribePlaceholder: "Your working email",
+        subscribe: "Subscribe",
+        footerDescription:
+            "Createx Online School is a leader in online studying. We have lots of courses and programs from the main market experts. We provide relevant approaches to online learning, internships and employment in the largest companies in the country.",
+        siteMap: "SITE MAP",
+        courses: "COURSES",
+        marketing: "Marketing",
+        management: "Management",
+        hrRecruting: "HR & Recruting",
+        design: "Design",
+        development: "Development",
+        contactUs: "CONTACT US",
+        signUpNewsLatter: "SIGN UP TO OUR NEWSLETTER",
+        signUpNewsLatterPlaceholder: "Email address",
+        signUpSubscribe:
+            "*Subscribe to our newsletter to receive communications and early updates from Createx SEO Agency.",
+        copyRight: "All rights reserved.",
+        madeWith: "Made with by Limon IT ",
+        backtoTop: "Back to top",
+    },
+    ru: {
+        langTitle: "RU",
+        flag: "Image/HomePagePhoto/twemoji--flag-russia.svg",
+        navAbout: "О нас",
+        navCourses: "Курсы",
+        navEvents: "Мероприятия",
+        navBlog: "Блог",
+        navContacts: "Контакты",
+        getConsultation: "Получить консультацию",
+        loginRegister: "Войти / Зарегистрироваться",
+        playShowreel: "Смотреть шоурил",
+        heroSectionH1:
+            "Получайте удовольствие от обучения на онлайн-курсах Createx",
+        exploreCourses: "Изучить курсы",
+        studentGraduet: "Выпускников",
+        commlatedCourses: "Пройденных курсов",
+        qualifiedTutors: "Квалифицированных преподавателей",
+        experienceYears: "Лет опыта",
+        whoweare: "Кто мы",
+        whyCreateX: "Почему Createx?",
+        why1: "A fermentum in morbi pretium aliquam adipiscing donec tempus.",
+        why2: "Vulputate placerat amet pulvinar lorem nisl.",
+        why3: "Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.",
+        why4: "Etiam duis lobortis in fames ultrices commodo nibh.",
+        why5: "Tincidunt sagittis neque sem ac eget.",
+        why6: "Ultricies amet justo et eget quisque purus vulputate dapibus tortor.",
+        readytolearn: "Готовы учиться?",
+        featuredCourses: "Рекомендуемые курсы",
+        viewAllCourses: "Посмотреть все курсы",
+        ourbenefits: "Наши преимущества",
+        thatshowwedoit: "Вот как мы это делаем",
+        experiencedTutors: "Опытные преподаватели",
+        feedbackSupports: "Обратная связь и поддержка",
+        onlineLibrary: "Онлайн-библиотека 24/7",
+        community: "Сообщество",
+        ourevents: "Наши мероприятия",
+        lecturesWorkshop: "Лекции и воркшопы",
+        doyouwatnmore: "Хотите большего?",
+        exploreAllEvents: "Изучить все мероприятия",
+        createXCertificate: "Сертификат Createx",
+        createXCertificateH1: "Ваш профессионализм будет подтвержден",
+        createXCertificateP:
+            "Мы аккредитованы международными профессиональными организациями и институтами:",
+        bestTutorsHere: "Все лучшие преподаватели здесь",
+        meetOurTema: "Встречайте нашу команду",
+        testimonials: "ОТЗЫВЫ",
+        whatOurStudentsSay: "Что говорят наши студенты",
+        ourBlog: "Наш блог",
+        latestPosts: "Последние публикации",
+        goBlog: "Перейти в блог",
+        dontmiss: "Ничего не упустите",
+        subscribeTitle: "Подпишитесь на анонсы школы Createx",
+        subscribePlaceholder: "Ваша рабочая эл. почта",
+        subscribe: "Подписаться",
+        footerDescription:
+            "Онлайн-школа Createx — лидер в сфере онлайн-обучения. У нас представлено множество курсов и программ от ведущих экспертов рынка. Мы предлагаем актуальные подходы к онлайн-обучению, стажировкам и трудоустройству в крупнейших компаниях страны.",
+        siteMap: "КАРТА САЙТА",
+        courses: "КУРСЫ",
+        marketing: "Маркетинг",
+        management: "Менеджмент",
+        hrRecruting: "HR и Рекрутинг",
+        design: "Дизайн",
+        development: "Разработка",
+        contactUs: "СВЯЗАТЬСЯ С НАМИ",
+        signUpNewsLatter: "ПОДПИШИТЕСЬ НА НАШУ РАССЫЛКУ",
+        signUpNewsLatterPlaceholder: "Адрес эл. почты",
+        signUpSubscribe:
+            "*Подпишитесь на нашу рассылку, чтобы получать сообщения и самые свежие обновления от SEO-агентства Createx.",
+        copyRight: "Все права защищены.",
+        madeWith: "Разработано в Limon IT",
+        backtoTop: "Наверх",
+    },
+    uz: {
+        langTitle: "UZ",
+        flag: "Image/HomePagePhoto/twemoji--flag-uzbekistan.svg",
+        navAbout: "Biz haqimizda",
+        navCourses: "Kurslar",
+        navEvents: "Tadbirlar",
+        navBlog: "Blog",
+        navContacts: "Kontaktlar",
+        getConsultation: "Konsultatsiya olish",
+        loginRegister: "Kirish / Ro'yxatdan o'tish",
+        playShowreel: "Shou-rolikni ko'rish",
+        heroSectionH1: "Createx Online Kurslari bilan o'qishdan zavqlaning",
+        exploreCourses: "Kurslarni o'rganish",
+        studentGraduet: "Bitirgan talabalar",
+        commlatedCourses: "Tugallangan kurslar",
+        qualifiedTutors: "Malakali o'qituvchilar",
+        experienceYears: "Yillik tajriba",
+        whoweare: "Biz kimmiz",
+        whyCreateX: "Nega aynan Createx?",
+        why1: "A fermentum in morbi pretium aliquam adipiscing donec tempus.",
+        why2: "Vulputate placerat amet pulvinar lorem nisl.",
+        why3: "Consequat feugiat habitant gravida quisque elit bibendum id adipiscing sed.",
+        why4: "Etiam duis lobortis in fames ultrices commodo nibh.",
+        why5: "Tincidunt sagittis neque sem ac eget.",
+        why6: "Ultricies amet justo et eget quisque purus vulputate dapibus tortor.",
+        readytolearn: "O'rganishga tayyormisiz?",
+        featuredCourses: "Saralangan Kurslar",
+        viewAllCourses: "Barcha kurslarni ko'rish",
+        ourbenefits: "Bizning afzalliklarimiz",
+        thatshowwedoit: "Biz buni mana shunday bajaramiz",
+        experiencedTutors: "Tajribali O'qituvchilar",
+        feedbackSupports: "Aloqa va Qo'llab-quvvatlash",
+        onlineLibrary: "24/7 Onlayn Kutubxona",
+        community: "Hamjamiyat",
+        ourevents: "Bizning Tadbirlarimiz",
+        lecturesWorkshop: "Ma'ruzalar va seminarlar",
+        doyouwatnmore: "Yana ko'proq bilishni xohlaysizmi?",
+        exploreAllEvents: "Barcha tadbirlarni ko'rish",
+        createXCertificate: "Createx Sertifikati",
+        createXCertificateH1: "Sizning tajribangiz tasdiqlanadi",
+        createXCertificateP:
+            "Biz xalqaro professional tashkilotlar va institutlar tomonidan akkreditatsiyadan o'tganmiz:",
+        bestTutorsHere: "Eng yaxshi o'qituvchilar shu yerda",
+        meetOurTema: "Jamoamiz bilan tanishing",
+        testimonials: "FIKRLAR",
+        whatOurStudentsSay: "Talabalarimiz nima deyishadi",
+        ourBlog: "Bizning blog",
+        latestPosts: "So'nggi postlar",
+        goBlog: "Blogga o'tish",
+        dontmiss: "Hech narsani o'tkazib yubormang",
+        subscribeTitle: "Createx maktabi e'lonlariga obuna bo'ling",
+        subscribePlaceholder: "Sizning ishchi elektron pochtangiz",
+        subscribe: "Obuna bo'lish",
+        footerDescription:
+            "Createx Online School — onlayn ta'lim sohasida yetakchi hisoblanadi. Bizda bozorning yetakchi ekspertlaridan ko'plab kurslar va dasturlar mavjud. Biz mamlakatning eng yirik kompaniyalarida onlayn ta'lim, amaliyot va ishga joylashish uchun eng dolzarb yondashuvlarni taqdim etamiz.",
+        siteMap: "SAYT XARITASI",
+        courses: "KURSLAR",
+        marketing: "Marketing",
+        management: "Menejment",
+        hrRecruting: "HR va Rekruting",
+        design: "Dizayn",
+        development: "Dasturlash",
+        contactUs: "BIZ BILAN ALOQA",
+        signUpNewsLatter: "YANGILIKLARIMIZGA OBUNA BO'LING",
+        signUpNewsLatterPlaceholder: "Elektron pochta manzili",
+        signUpSubscribe:
+            "*Createx SEO agentligidan xabarlar va eng so'nggi yangiliklarni olish uchun xabarnomamizga obuna bo'ling.",
+        copyRight: "Barcha huquqlar himoyalangan.",
+        madeWith: "Limon IT tomonidan yaratildi",
+        backtoTop: "Tepaga qaytish",
+    },
+};
+let currentLang = localStorage.getItem("selectedLang") || "en";
+
+
+const headerMain = document.querySelector("#headerMain");
+
+headerMain.innerHTML = `
+    <div
+        class="container mx-auto px-5 md:px-10 lg:px-20 flex justify-between items-center py-5"
+    >
+        <div class="flex items-center gap-7 md:gap-10">
+            <div class="">
+                <a href="#">
+                    <img
+                        src="./Image/HomePagePhoto/logo.svg"
+                        alt="logo image"
+                    />
+                </a>
+            </div>
+            <nav class="hidden lg:flex items-center gap-4 xl:gap-8">
+                <a
+                    href="./Pages/About/about.html"
+                    class="font-[Lato] font-bold text-[16px] leading-[160%] text-[#424551] hover:text-[#FF3F3A] transition-all duration-300"
+                >${translateData[currentLang].navAbout}</a>
+                <a
+                    href="./Pages/Courses/courses.html"
+                    class="font-[Lato] font-bold text-[16px] leading-[160%] text-[#424551] hover:text-[#FF3F3A] transition-all duration-300"
+                >${translateData[currentLang].navCourses}</a>
+                <a
+                    href="./Pages/Events/events.html"
+                    class="font-[Lato] font-bold text-[16px] leading-[160%] text-[#424551] hover:text-[#FF3F3A] transition-all duration-300"
+                >${translateData[currentLang].navEvents}</a>
+                <a
+                    href="./Pages/Blogs/blogs.html"
+                    class="font-[Lato] font-bold text-[16px] leading-[160%] text-[#424551] hover:text-[#FF3F3A] transition-all duration-300"
+                >${translateData[currentLang].navBlog}</a>
+                <a
+                    href="./Pages/Contacts/contacts.html"
+                    class="font-[Lato] font-bold text-[16px] leading-[160%] text-[#424551] hover:text-[#FF3F3A] transition-all duration-300"
+                >${translateData[currentLang].navContacts}</a>
+            </nav>
+        </div>
+        <div class="flex items-center gap-3 md:gap-4">
+            <div class="flex items-center gap-3 md:gap-4">
+                <a
+                    href="./Pages/Contacts/contacts.html"
+                    class="font-[Lato] font-bold text-[12px] lg:py-1 md:leading-6 lg:leading-4 tracking-[0.5px] text-white bg-gradient-to-r from-[#FF3F3A] to-[#F75E05] px-5 lg:px-3 rounded-[4px] hidden sm:inline-block hover:bg-gradient-to-l hover:from-[#F75E05] hover:to-[#FF3F3A]"
+                >
+                    ${translateData[currentLang].getConsultation}
+                </a>
+                <a
+                    href="#!"
+                    class="hidden lg:flex gap-2 items-center font-[Lato] font-bold text-3 lg:text-2 lg:leading-4 text-gray-800"
+                >
+                    <img
+                        src="./Image/HomePagePhoto/Profile.svg"
+                        alt="profile icon"
+                        class="hidden xl:block"
+                    />
+                    ${translateData[currentLang].loginRegister}
+                </a>
+            </div>
+            <div class="hidden min-[300px]:block relative">
+                <button
+                    class=" flex items-center gap-1 px-2 rounded-[20px]"
+                    id="langBtn"
+                ></button>
+
+                <div
+                    class="bg-[#F75E05] flex-col items-center gap-1.5 py-1.5 rounded-[10px] absolute top-[30px] w-full hidden"
+                    id="langModal"
+                ></div>
+            </div>
+            <button class="block lg:hidden">
+                <img
+                    src="./Image/HomePagePhoto/stash--burger-classic-light.svg"
+                    alt="burger icon"
+                />
+            </button>
+        </div>
+    </div>
+`;
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
     if (window.scrollY > 300) {
-        header.classList.add('fixed', 'top-0', 'left-0', 'bg-white', 'shadow-[0px_8px_8px_0px_rgba(0,0,0,0.1)]', 'z-10')
-        header.classList.remove('absolute')
+        header.classList.add(
+            "fixed",
+            "top-0",
+            "left-0",
+            "bg-white",
+            "shadow-[0px_8px_8px_0px_rgba(0,0,0,0.1)]",
+            "z-10",
+        );
+        header.classList.remove("absolute", "bg-transparent");
     } else {
-        header.classList.remove('fixed', 'top-0', 'left-0', 'bg-white', 'shadow-[0px_8px_8px_0px_rgba(0,0,0,0.1)]', 'z-10')
-        header.classList.add('absolute')
+        header.classList.remove(
+            "fixed",
+            "top-0",
+            "left-0",
+            "bg-white",
+            "shadow-[0px_8px_8px_0px_rgba(0,0,0,0.1)]",
+            "z-10",
+        );
+        header.classList.add("absolute", "bg-transparent");
     }
-})
+});
+
+
+
+const langBtn = document.querySelector("#langBtn");
+const langModal = document.querySelector("#langModal");
+
+const selectedLang = localStorage.getItem("selectedLang");
+
+if (!localStorage.getItem("selectedLang")) {
+    localStorage.setItem("selectedLang", currentLang);
+}
+mainLangButton();
+
+function updateLangModal() {
+    langModal.innerHTML = "";
+
+    Object.keys(translateData).forEach((langKey) => {
+        if (langKey !== currentLang) {
+            const targetLang = langKey;
+
+            const langModalOption = document.createElement("button");
+            langModalOption.className =
+                "w-full flex items-center justify-center gap-1.5 group hover:bg-[#FF3F3A] rounded transition-all duration-300";
+            langModalOption.innerHTML = `
+                <img
+                    src="${translateData[targetLang].flag}"
+                    alt="language button icon"
+                />
+                <span class="text-white text-[12px]">${translateData[targetLang].langTitle}</span>
+            `;
+
+            langModalOption.addEventListener("click", () => {
+                localStorage.setItem("selectedLang", targetLang);
+                currentLang = targetLang;
+                mainLangButton();
+                updateLangModal();
+                window.location.reload();
+            });
+            langModal.appendChild(langModalOption);
+        }
+    });
+}
+function mainLangButton() {
+    langBtn.innerHTML = `
+        <img
+            src="${translateData[currentLang].flag}"
+            alt="language button icon"
+        />
+        <span class="downSpan transition-all duration-300">
+            <img
+                src="./Image/HomePagePhoto/icon-park-solid--down-one.svg"
+                alt="down icon"
+            />
+        </span>
+    `;
+}
+
+langBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const downSpan = document.querySelector(".downSpan");
+    langModal.classList.toggle("hidden");
+    langModal.classList.toggle("flex");
+    downSpan.classList.toggle("rotate-180");
+});
+document.addEventListener("click", () => {
+    langModal.classList.add("hidden");
+    langModal.classList.remove("flex");
+    const downSpan = document.querySelector(".downSpan");
+    if (downSpan) downSpan.classList.remove("rotate-180");
+});
+updateLangModal();
+
 
