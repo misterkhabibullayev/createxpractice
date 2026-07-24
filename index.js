0f6a40e4028e43163a62527eecf45fcaf85397cf
misterkhabibullayev <misterkhabibullayev1@gmail.com>
2026-07-24 11:42:31 +0500

    f

--- index.js
+++ index.js
@@ -929,6 +929,7 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".custom-prev-btn",
     },
     breakpoints: {
+        320: { slidesPerView: 1, spaceBetween: 20 },
         640: { slidesPerView: 2, spaceBetween: 20 },
         768: { slidesPerView: 3, spaceBetween: 25 },
         1024: { slidesPerView: 4, spaceBetween: 30 },
